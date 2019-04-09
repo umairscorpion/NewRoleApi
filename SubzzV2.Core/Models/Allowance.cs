c6238acbac22906a3ef79208e7e888c24c3f3aba
Taimoor <tamor1972@hotmail.com>
2019-04-09 23:07:39 +0500

    Allowances

---
+++ SubzzV2.Core/Models/Allowance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubzzV2.Core.Models
+{
+    public class Allowance
+    {
+        public int Id { get; set; }
+        public int DistrictId { get; set; }
+        public string Title { get; set; }
+        public string YearlyAllowance { get; set; }
+        public bool IsDeductAllowance { get; set; }
+        public bool IsResidualDays { get; set; }
+        public bool IsEnalbled { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
